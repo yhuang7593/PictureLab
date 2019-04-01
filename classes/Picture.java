591489fb2a38d9f8ed9a6343f8fc643c27dc225c
BT_1N3_27 <kfang9357@bths.edu>
2019-04-01 08:34:14 -0400

    added copy 2

--- classes/Picture.java
+++ classes/Picture.java
@@ -342,6 +342,29 @@ public class Picture extends SimplePicture
     }   
   }
 
+    public void copy2(Picture fromPic, int startRow, int endRow, int startCol, int endCol) {
+        Pixel fromPixel = null;
+        Pixel toPixel = null;
+        Pixel[][] toPixels = this.getPixels2D();
+        Pixel[][] fromPixels = fromPic.getPixels2D();
+
+        for (int fromRow = 0, toRow = startRow;
+             fromRow < fromPixels.length &&
+                     toRow < endRow;
+             fromRow++, toRow++)
+        {
+            for (int fromCol = 0, toCol = startCol;
+                 fromCol < fromPixels[0].length &&
+                         toCol < endCol;
+                 fromCol++, toCol++)
+            {
+                fromPixel = fromPixels[fromRow][fromCol];
+                toPixel = toPixels[toRow][toCol];
+                toPixel.setColor(fromPixel.getColor());
+            }
+        }
+    }
+
   /** Method to create a collage of several pictures */
   public void createCollage()
   {
